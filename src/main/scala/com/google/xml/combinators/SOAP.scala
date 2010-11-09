12ad54d57dffc35e669865a7a31638f2a4a8e516
searler <eggsearler@verizon.net>
2010-11-08 19:02:43 -0600

    tidy

--- src/main/scala/com/google/xml/combinators/SOAP.scala
+++ src/main/scala/com/google/xml/combinators/SOAP.scala
@@ -7,12 +7,9 @@ object SoapDocLiteral {
 
   import Picklers._
 
-  final val SOAP = URI("http://www.w3.org/2003/05/soap-envelope")
+  final val SOAP = URI(javax.xml.soap.SOAPConstants.URI_NS_SOAP_1_2_ENVELOPE)
    
-  def rawPickler[T](cp:Pickler[T])  = 
-    elem(SOAP , "Envelope", 
-      elem(SOAP, "Body", cp)  
-        )
+  def rawPickler[T](cp:Pickler[T])  = elem(SOAP , "Envelope", elem(SOAP, "Body", cp))
   
    private def fromSoapDocLiteral[T](n:SoapDocLiteral[T]) = Some(SoapDocLiteral.unapply[T](n).get)
 
