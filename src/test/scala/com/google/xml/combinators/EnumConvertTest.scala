82e6a506e2b3ff7e10f57ac283e82d97fb08e606
searler <eggsearler@verizon.net>
2010-11-08 21:53:28 -0600

    tidy

---
+++ src/test/scala/com/google/xml/combinators/EnumConvertTest.scala
@@ -0,0 +1,37 @@
+package com.google.xml.combinators
+
+import org.specs._
+
+
+
+import Picklers._
+
+object EnumConvertTest extends PicklerAsserts{
+  
+  "unpickle" in {
+    val r = new EnumContainer("name",TestEnum.BETA)
+   val out = PlainOutputStore.empty
+    val xml=   EnumContainer.pickler.pickle(r,out)
+    """<container xmlns="testing-uri">
+<name>name</name>
+<enum>BETA</enum>
+</container>
+""" must beEqualTo(normalize(xml.document))
+
+   }
+
+"pickle" in {
+    val in = """<container xmlns="testing-uri">
+<name>name</name>
+<enum>BETA</enum>
+</container>"""
+            
+     val result = EnumContainer.pickler.unpickle(LinearStore.fromString(in))
+     
+      result match {
+      case Success(v, _) => EnumContainer("name",TestEnum.BETA) must beEqualTo(v)
+      case f: NoSuccess  => fail(f toString)
+    }
+}
+
+}
