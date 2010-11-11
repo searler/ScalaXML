48eb11df3c6b03d63bb6c7358a9b74730a3ea3c2
searler <eggsearler@verizon.net>
2010-11-11 17:51:16 -0600

    eliminate redundancy

--- src/test/scala/com/google/xml/combinators/SimpleMapContainerTest.scala
+++ src/test/scala/com/google/xml/combinators/SimpleMapContainerTest.scala
@@ -12,20 +12,26 @@ import Picklers._
 
 class MapContainedTest  extends PicklerAsserts{
 
+val in = """<container xmlns="testing-uri">
+<name>name</name>
+<key>tagged</key>
+<value>123</value>
+<key>next</key>
+<value>1623</value>
+</container>
+"""
+
+val value = SimpleMapContainer("name",Map("tagged"->MapContained("tagged",123),"next"->MapContained("next",1623)))
    
 
 
  "parseContained" in {
-    val in = """<container xmlns="testing-uri">
-<name>name</name>
-<key>tagged</key>
-<value>123</value>
-</container>"""
+    
             
      val result = SimpleMapContainer.pickler.unpickle(LinearStore.fromString(in))
      
       result match {
-      case Success(v, _) => SimpleMapContainer("name",Map("tagged"->MapContained("tagged",123))) must beEqualTo(v)
+      case Success(v, _) => value must beEqualTo(v)
       case f: NoSuccess  => fail(f toString)
     }
 }
@@ -34,18 +40,10 @@ class MapContainedTest  extends PicklerAsserts{
 
 
  "unparseContained" in {
- val r=  SimpleMapContainer("name",Map("tagged"->MapContained("tagged",123),"next"->MapContained("next",1623)))
     
     val out = PlainOutputStore.empty
-    val xml=   SimpleMapContainer.pickler.pickle(r,out)
-    """<container xmlns="testing-uri">
-<name>name</name>
-<key>tagged</key>
-<value>123</value>
-<key>next</key>
-<value>1623</value>
-</container>
-""" must beEqualTo(normalize(xml.document))
+    val xml=   SimpleMapContainer.pickler.pickle(value,out)
+    in must beEqualTo(normalize(xml.document))
 
   }
       
